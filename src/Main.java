791afa9bea93ead4b48f533fcff7a8ff5527f720
LeonardVolodarchuk <126458141+LeonardVolodarchuk@users.noreply.github.com>
2023-05-15 03:49:43 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         Triangle triangle1 = new Triangle(1,2,3);
